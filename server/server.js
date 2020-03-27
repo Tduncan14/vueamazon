8722828661a7b8ba926473665c5636bf1ef4c98c
Treek <nbatre1@yahoo.com>
2020-03-27 10:53:40 -0500

    keep moving forward

--- server/server.js
+++ server/server.js
@@ -5,15 +5,41 @@ const bodyParser= require('body-parser');
 
 
 
-const App = express();
+const app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+
+app.get('/',(req,res) =>{
+
+  res.send('hello world')
+})
+
+
+app.post('/',(req,res) => {
+
+   console.log(req.body.name)
+
+})
+
+
+const PORT = process.env.PORT || 3000
 
 
 
-const PORT = process.env.PORT || PORT
 
 
 
-App.listen(PORT,()=>{
+
+
+
+app.listen(PORT,(err)=>{
+
+    if(err){
+        console.log(err)
+    }
 
    console.log(`listening to ${PORT}`)
 })

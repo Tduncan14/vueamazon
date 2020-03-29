af8e9a4a07183e00d54f8ce86eb63809cd87de7e
Treek <nbatre1@yahoo.com>
2020-03-29 10:57:44 -0500

    keep moving forward

---
+++ server/routes/product.js
@@ -0,0 +1,61 @@
+ const express = require('express');
+ const Product = require('../models/product');
+
+
+ const router = express.Router();
+
+//  title:String,
+//  description:String,
+//  photo:String,
+//  price:Number,
+//  stockQuantity:Number,
+//  rating:[Number]
+
+// post request -create a new product
+
+router.post('/products', async(req,res) =>{
+  try{
+      let product = new Product();
+
+      product.title = req.body.title;
+      product.description = req.body.description;
+      product.photo = req.body.photo;
+      product.stockQuantity = req.body.stockQuantity;
+      product.price = req.body.price;
+
+
+      await product.save()
+
+
+      res.json({
+          status:true,
+          message:"product is saved"
+      })
+
+
+  }
+  catch(err){
+
+    res.status(500).json({
+        success: false,
+        message:err.message
+
+    })
+
+  }
+})
+
+
+
+// get request which will get all the products
+
+
+
+// get request - will get a single product
+
+
+
+// put request - will update a single roduct
+
+
+// delete request = delete a single product

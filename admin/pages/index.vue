debb1d6403dcb15c5e61ac92fd6ae1c475ba7cb3
Treek <nbatre1@yahoo.com>
2020-04-02 15:36:36 -0500

    keep moving forward

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -8,6 +8,8 @@
          <h1 class="a-size-large a-spacing-none a-text-normal">
            All products
          </h1>
+          <div class="a-spacing-large"> </div>
+          <!-- buttons -->
          <a href="#" class="a-button-buy-again">
            Add a new product
          </a>
@@ -17,6 +19,8 @@
          <a href="#" class="a-button-history margin-right-10">
            Add a new product
          </a>
+
+         <!-- listings page -->
        </div>
 
      </div>
@@ -31,6 +35,175 @@
 
   </div>
 
+
+   <div class="container-fluid browsing-history">
+    <div class="row">
+        <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 br bb">
+      <div class="history-box">
+        <!-- product image -->
+        <a href="
+        #" class="a-link-normal">
+         <img src="#" class="img-fluid"/>
+        </a>
+
+      <!-- product title -->
+        <div class="a-spacing-top-base asin-title">
+          <span class="a-text-normal">
+            <div class="p13n-sc-truncated">Product Title</div>
+          </span>
+        </div>
+          <!-- product rating -->
+        <div class="a-row">
+          <a href="
+          #">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"> </i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          </a>
+          <span class="a-letter-space">
+
+          </span>
+           <span class="a-color-tertiary a-size-small asin-reviews">
+            (1732)
+          </span>
+        </div>
+        <!-- product price  -->
+        <div class="a-row">
+         <span class="a-size-base a-color-price">
+           <span class="p13n-sc-price">
+             $23
+           </span>
+         </span>
+        </div>
+      </div>
+      </div>
+         <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 br bb">
+      <div class="history-box">
+        <!-- product image -->
+        <a href="
+        #" class="a-link-normal">
+         <img src="#" class="img-fluid"/>
+        </a>
+
+      <!-- product title -->
+        <div class="a-spacing-top-base asin-title">
+          <span class="a-text-normal">
+            <div class="p13n-sc-truncated">Product Title</div>
+          </span>
+        </div>
+          <!-- product rating -->
+        <div class="a-row">
+          <a href="
+          #">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"> </i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          </a>
+          <span class="a-letter-space">
+
+          </span>
+           <span class="a-color-tertiary a-size-small asin-reviews">
+            (1732)
+          </span>
+        </div>
+        <!-- product price  -->
+        <div class="a-row">
+         <span class="a-size-base a-color-price">
+           <span class="p13n-sc-price">
+             $23
+           </span>
+         </span>
+        </div>
+      </div>
+      </div>
+         <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 br bb">
+      <div class="history-box">
+        <!-- product image -->
+        <a href="
+        #" class="a-link-normal">
+         <img src="#" class="img-fluid"/>
+        </a>
+
+      <!-- product title -->
+        <div class="a-spacing-top-base asin-title">
+          <span class="a-text-normal">
+            <div class="p13n-sc-truncated">Product Title</div>
+          </span>
+        </div>
+          <!-- product rating -->
+        <div class="a-row">
+          <a href="
+          #">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"> </i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          </a>
+          <span class="a-letter-space">
+
+          </span>
+           <span class="a-color-tertiary a-size-small asin-reviews">
+            (1732)
+          </span>
+        </div>
+        <!-- product price  -->
+        <div class="a-row">
+         <span class="a-size-base a-color-price">
+           <span class="p13n-sc-price">
+             $23
+           </span>
+         </span>
+        </div>
+      </div>
+      </div>
+         <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 br bb">
+      <div class="history-box">
+        <!-- product image -->
+        <a href="
+        #" class="a-link-normal">
+         <img src="#" class="img-fluid"/>
+        </a>
+
+      <!-- product title -->
+        <div class="a-spacing-top-base asin-title">
+          <span class="a-text-normal">
+            <div class="p13n-sc-truncated">Product Title</div>
+          </span>
+        </div>
+          <!-- product rating -->
+        <div class="a-row">
+          <a href="
+          #">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"> </i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          </a>
+          <span class="a-letter-space">
+
+          </span>
+           <span class="a-color-tertiary a-size-small asin-reviews">
+            (1732)
+          </span>
+        </div>
+        <!-- product price  -->
+        <div class="a-row">
+         <span class="a-size-base a-color-price">
+           <span class="p13n-sc-price">
+             $23
+           </span>
+         </span>
+        </div>
+      </div>
+      </div>
+    </div>
+   </div>
 </main>
  
 </template>
